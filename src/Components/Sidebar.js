0f3d2a043701c7a6b9f1ff42b4bc5625a64547d7
Emde <mortenemdec@gmail.com>
2020-06-02 11:17:28 +0200

    update style for iphone X and galaxy 6 and change layout of content on large desktop screens

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,29 +4,27 @@ import logo from '../Styles/Images/logo512.png';
 
 function Sidebar() {
   return (
-    <div className="Sidebar">
-      <aside className="CVSidebar">
-        <img className="HeadShot" src={logo} alt="Logo" />
-        <h2 className="ContactHeader">CONTACT</h2>
-        <ul className="ContactList">
-          <li className="ContactItem">
-            +31 (0)613993521
-          </li>
-          <li className="ContactItem">
-            mortenemdec@gmail.com
-          </li>
-          <li className="ContactItem">
-            George Gershwinlaan 519,<br/>Amsterdam,<br/>The Netherlands
-          </li>
-          <li className="ContactItem">
-            <a href="https://github.com/MortenEmde">GitHub</a>
-          </li>
-          <li className="ContactItem">
-            <a href="https://linkedin.com/in/morten-emde-christensen">LinkedIn</a>
-          </li>
-        </ul>
-      </aside>
-    </div>
+    <aside className="Sidebar">
+      <img className="HeadShot" src={logo} alt="Logo" />
+      <h2 className="ContactHeader">CONTACT</h2>
+      <ul className="ContactList">
+        <li className="ContactItem">
+          +31 (0)613993521
+        </li>
+        <li className="ContactItem">
+          mortenemdec@gmail.com
+        </li>
+        <li className="ContactItem">
+          George Gershwinlaan 519,<br/>Amsterdam,<br/>The Netherlands
+        </li>
+        <li className="ContactItem">
+          <a href="https://github.com/MortenEmde">GitHub</a>
+        </li>
+        <li className="ContactItem">
+          <a href="https://linkedin.com/in/morten-emde-christensen">LinkedIn</a>
+        </li>
+      </ul>
+    </aside>
   );
 }
 
